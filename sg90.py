ff353f03b7985543bf56ba3831c60f91b6c82709
Simone Candido <59175940+SimoneMoreWare@users.noreply.github.com>
2022-07-24 14:53:40 +0200

    Create sg90.py

---
+++ sg90.py
@@ -0,0 +1,23 @@
+from gpiozero import Servo
+from time import sleep
+from gpiozero.pins.pigpio import PiGPIOFactory
+
+pigpio_factory = PiGPIOFactory()
+
+servo = Servo(22, pin_factory=pigpio_factory)
+servo.mid()
+print("servo mid")
+sleep(3)
+
+while True:
+  servo.min()
+  print("servo min")
+  sleep(3)
+
+  servo.mid()
+  print("servo mid")  
+  sleep(3)
+
+  servo.max()
+  print("servo max")
+  sleep(3)
